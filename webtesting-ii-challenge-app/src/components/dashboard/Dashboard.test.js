cd647228eee674f36965eb7127e9946773bed500
ParrishJ <jarparrish@gmail.com>
2019-06-18 14:58:24 -0700

    basic tests for display added

--- webtesting-ii-challenge-app/src/components/dashboard/Dashboard.test.js
+++ webtesting-ii-challenge-app/src/components/dashboard/Dashboard.test.js
@@ -3,3 +3,19 @@ import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
 
 import Dashboard from './Dashboard'
+
+describe('<Dashboard />', () => {
+    describe('Strike Button', () => {
+        it('Should increase strikes by one ', () => {
+            const { getByText } = render(<Dashboard />);
+
+            const strikeButton = getByText(/strike/i);
+
+            fireEvent.click(strikeButton);
+
+
+        });
+
+    });
+
+})
--- webtesting-ii-challenge-app/src/components/display/Display.test.js
+++ webtesting-ii-challenge-app/src/components/display/Display.test.js
@@ -3,3 +3,29 @@ import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
 
 import Display from './Display'
+
+describe('<Display />', () => {
+    it('Should display the word "Strikes" ', () => {
+        const { getByText } = render(<Display />);
+
+        getByText(/strikes/i);
+    });
+
+    it('Should display the word "balls" ', () => {
+        const { getByText } = render(<Display />);
+
+        getByText(/balls/i);
+    });
+
+    it('Should display the word "fouls" ', () => {
+        const { getByText } = render(<Display />);
+
+        getByText(/fouls/i);
+    });
+
+    it('Should display the word "hits" ', () => {
+        const { getByText } = render(<Display />);
+
+        getByText(/hits/i);
+    });
+});
